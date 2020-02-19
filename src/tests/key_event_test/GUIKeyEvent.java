74294a20d74f01d8f3d89b0951ddef7a5e0e27c3
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-02-19 12:25:37 +0100

    GUIKeyEvent.java GUI fixed and KeyListener methods implemented

--- src/tests/key_event_test/GUIKeyEvent.java
+++ src/tests/key_event_test/GUIKeyEvent.java
@@ -9,38 +9,75 @@ import java.awt.event.KeyListener;
  * Made following http://ecomputernotes.com/java/swing/keyevent
  */
 public class GUIKeyEvent extends JFrame implements KeyListener {
-    private JLabel LblOutPt = new JLabel("Key Typed is:");
-    private JTextField Txt = new JTextField(10);
+    private JLabel labelKeyTyped = new JLabel();
+    private JLabel labelKeyPressed = new JLabel();
+    private JLabel labelKeyReleased = new JLabel();
+    JTextField textField;
+    JLabel labelPrompt;
+    JPanel labelPanel;
+    JPanel labelNorth;
 
     public GUIKeyEvent() {
         setTitle("Java Example of Key Event");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
-        JLabel lblPrmpt = new JLabel("Press keys as Desired Below:");
-        add(lblPrmpt, BorderLayout.NORTH);
-        add(Txt, BorderLayout.CENTER);
-        add(LblOutPt, BorderLayout.SOUTH);
+
+        textField = new JTextField(10);
+        labelPrompt = new JLabel("Press any keys:");
+        labelPanel = new JPanel();
+        labelNorth = new JPanel();
+        labelPanel.setLayout(new GridLayout(3, 1));
+        labelNorth.setLayout(new GridLayout(2, 1));
+
+        add(labelNorth, BorderLayout.NORTH);
+        labelNorth.add(labelPrompt);
+        labelNorth.add(textField);
+
+        add(labelPanel, BorderLayout.CENTER);
+        labelPanel.add(labelKeyTyped);
+        labelPanel.add(labelKeyPressed);
+        labelPanel.add(labelKeyReleased);
+
         addKeyListener(this);
-        Txt.addKeyListener(this);
+        textField.addKeyListener(this);
+        setVisible(true);
     }
 
-    public static void main(String[] ar) {
-        GUIKeyEvent Frm = new GUIKeyEvent();
-        final int WIDTH = 500;
-        final int HEIGHT = 500;
-        Frm.setSize(WIDTH, HEIGHT);
-        Frm.setVisible(true);
+    public static void main(String[] args) {
+        // set look and feel to the system look and feel
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        GUIKeyEvent gui = new GUIKeyEvent();
+        // close the operation of JFrame
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // set size of JFrame.
+        gui.setSize(500, 500);
+        // set visible status of JFrame
+        gui.setVisible(true);
+
+        // SwingUtilities.invokeLater(() -> new FactoryShaper().setVisible(true));
     }
 
-    public void keyTyped(KeyEvent Evnt) {
-        char s = Evnt.getKeyChar();
-        LblOutPt.setText("Last key Pressed:" + s);
+    public void keyTyped(KeyEvent keyEvent) {
+        char keyChar = keyEvent.getKeyChar();
+        int keyCode = keyEvent.getKeyCode();
+
+        labelKeyTyped.setText("|keyTyped| getKeyChar: " + keyChar);
+        labelKeyTyped.setText("|keyTyped| getKeyCode: " + keyCode);
     }
 
     public void keyPressed(KeyEvent Evnt) {
+        char keyChar = Evnt.getKeyChar();
+        labelKeyPressed.setText("|keyPressed| getKeyChar: " + keyChar);
     }
 
     public void keyReleased(KeyEvent Evnt) {
+        char keyChar = Evnt.getKeyChar();
+        labelKeyReleased.setText("|keyReleased| getKeyChar: " + keyChar);
     }
 }
 
