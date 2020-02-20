e3067061bf34ce53a5be1201cb86d9be11e7d963
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-02-20 09:58:33 +0100

    Pairs added to pairArrayList

--- src/tests/pair/PairMorseTest.java
+++ src/tests/pair/PairMorseTest.java
@@ -8,11 +8,48 @@ import java.util.Arrays;
 public class PairMorseTest {
     public final static ArrayList<Pair<Integer, String>> pairArrayList =
             new ArrayList<>(Arrays.asList(
-                    new Pair<>(65, ".-"),
+                    new Pair<>(46, ".-.-.-"), // dot
+                    new Pair<>(44, "--..--"), // comma
+                    new Pair<>(65, ".-"), // A
                     new Pair<>(66, "-..."),
                     new Pair<>(67, "-.-."),
                     new Pair<>(68, "-.."),
-                    new Pair<>(69, ".")
+                    new Pair<>(69, "."),
+                    new Pair<>(70, "..-."),
+                    new Pair<>(71, "--."),
+                    new Pair<>(72, "...."),
+                    new Pair<>(73, ".."),
+                    new Pair<>(74, ".---"),
+                    new Pair<>(75, "-.-"),
+                    new Pair<>(76, ".-.."),
+                    new Pair<>(77, "--"),
+                    new Pair<>(78, "-."),
+                    new Pair<>(79, "---"),
+                    new Pair<>(80, ".--."),
+                    new Pair<>(81, "--.-"),
+                    new Pair<>(82, ".-."),
+                    new Pair<>(83, "..."),
+                    new Pair<>(84, "-"),
+                    new Pair<>(85, "..-"),
+                    new Pair<>(86, "...-"),
+                    new Pair<>(87, ".--"),
+                    new Pair<>(88, "-..-"),
+                    new Pair<>(89, "-.--"),
+                    new Pair<>(90, "--.."), // Z
+                    // numeric pad numbers
+                    new Pair<>(96, "-----"), // 0
+                    new Pair<>(97, ".----"), // 1
+                    new Pair<>(98, "..---"),
+                    new Pair<>(99, "...--"),
+                    new Pair<>(100, "....-"),
+                    new Pair<>(101, "....."), // 5
+                    new Pair<>(102, "-...."),
+                    new Pair<>(103, "--..."),
+                    new Pair<>(104, "---.."),
+                    new Pair<>(105, "----."), // 9
+
+                    new Pair<>(111, "-..-."), // '/'
+                    new Pair<>(222, "..--..") // question mark
                     ));
 
     public static Pair<Integer, String> getMaximum(ArrayList<Pair<Integer, String>> list) {
